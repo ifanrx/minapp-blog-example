561ff546f00245615e93dc16cac61fb425d8ef8f
Jiajun Yan <yanjiajun@ifanr.com>
2021-09-06 17:07:45 +0800

    feat: 接入知晓云

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import BaaS from 'minapp-sdk'
 import './index.css'
 import App from './App'
+
+BaaS.init('a4d2d62965ddb57fa4d6', { env: '2dff565f980ee7a7096c' })
 
 ReactDOM.render(
   <React.StrictMode>
